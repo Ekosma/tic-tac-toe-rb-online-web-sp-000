1f6c0fa154e9c9cdb8f2ae9acea73ecf88030428
Evelyn Kosma <ezranaja@gmail.com>
2021-02-18 18:17:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,3 +55,49 @@ def turn (board)
     turn(board)
   end
 end
+
+def play(board)
+  i = 0
+  while i < 9
+    i += 1
+    turn(board)
+  end
+end
+
+#Tic Tac Toe Play loop
+
+def turn_count(turn)
+  move_count = []
+  turn.each do |unit|
+    if unit == "X" || unit == "O"
+      move_count.push(1)
+    end
+  end
+  move_count.length.to_i
+end
+
+def current_player(board)
+    num = turn_count(board)
+    if num % 2 == 0
+      return "X"
+    else
+      return "O"
+    end
+=======
+
+#Tic Tac Toe Play loop
+
+def turn_count(board)
+  counter = 0
+  board.each do |spaces|
+    if spaces == "X" || spaces == "O"
+      Counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "0"
+>>>>>>> 1180beb0e3e0e22f613e5e7092d4f3c8d6d8d943
+end
